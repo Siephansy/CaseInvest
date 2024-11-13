a68721d863cc7802b4495fbb1336b44c5b3794f8
Phelipe Braga <phelipebc@terra.com.br>
2024-11-13 19:14:41 -0300

    19
    19

--- pages/MapaMacDonalds.py
+++ pages/MapaMacDonalds.py
@@ -25,3 +25,5 @@ st_folium(map, width=700, height=500)
 google_maps_iframe = """
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5416344698983!2d-46.656497985021065!3d-23.561531484682776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce58583d14c6b7%3A0x9c6fb81eb64a5bb1!2sMcDonald's%20-%20Av.%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1605463652291!5m2!1spt-BR!2sbr" width="700" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 """
+# Exibindo o mapa no Streamlit
+st.markdown(google_maps_iframe, unsafe_allow_html=True)
