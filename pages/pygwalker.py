2d658993973c2c5a598ba098963be52a81879c65
Phelipe Braga <phelipebc@terra.com.br>
2025-02-17 15:45:32 -0300

    117
    117

--- pages/pygwalker.py
+++ pages/pygwalker.py
@@ -1,4 +1,3 @@
-from pygwalker.api.streamlit import StreamlitRenderer
 import pandas as pd
 import streamlit as st
 import os
@@ -14,13 +13,14 @@ st.title("Use Pygwalker In Streamlit")
 
 # You should cache your pygwalker renderer, if you don't want your memory to explode
 @st.cache_resource
-def get_pyg_renderer() -> StreamlitRenderer:
+def get_pyg_renderer():
     csv_path = "./bike_sharing_dc.csv"
     if not os.path.exists(csv_path):
         st.error(f"CSV file not found: {csv_path}")
         return None
     df = pd.read_csv(csv_path)
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
+    from pygwalker.api.streamlit import StreamlitRenderer
     return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw")
 
 renderer = get_pyg_renderer()
